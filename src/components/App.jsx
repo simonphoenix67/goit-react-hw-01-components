152d71ab98263fc84ece9477ce8b77d73d0899f7
simonphoenix67 <simonphoenix67@gmail.com>
2023-03-12 16:04:13 +0000

    2/4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Profile } from './profile/profile';
-import user from './data/user.json'
+import user from './data/user.json';
 
+import { Statistics } from './Statistics/Statistics';
+import data from './data/data.json';
 
 export const App = () => {
   return (
@@ -22,6 +24,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
     </div>
   );
 };
